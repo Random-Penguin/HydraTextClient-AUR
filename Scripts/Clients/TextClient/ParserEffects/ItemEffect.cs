3e220de80d6aff62991a7116a7370ac450dc1249
SWCreeperKing <sw.creeperking@gmail.com>
2026-07-22 02:45:44 -0700

    actually fix show only related to you, and fix #47

--- Scripts/Clients/TextClient/ParserEffects/ItemEffect.cs
+++ Scripts/Clients/TextClient/ParserEffects/ItemEffect.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Archipelago.MultiClient.Net.Enums;
@@ -17,7 +18,7 @@ public class ItemEffect : MessageParserEffect
     public const string SaveId = "Clients/TextClient/TextEffects/ItemMessageEffect";
     public const string Default = "[{{img}}{{name}}]";
 
-    private static Dictionary<string, IPrintableObj[]> CustomAssetsItemCache = [];
+    private static ConcurrentDictionary<string, Texture2D> CustomAssetsItemCache = [];
 
     public override string Key => "item";
 
@@ -31,7 +32,7 @@ public class ItemEffect : MessageParserEffect
                 argList[0] = $"{argList[0]};{argList[1]}";
                 argList.RemoveAt(1);
             }
-            
+
             while (!argList[1].EndsWith("``"))
             {
                 argList[1] = $"{argList[1]};{argList[2]}";
@@ -44,7 +45,7 @@ public class ItemEffect : MessageParserEffect
             label.AddText("[Invalid Item Tag]");
             return;
         }
-        
+
         if (args.Length < 2 || reloadFunction is null)
         {
             label.AddText("[Invalid Item Tag]");
@@ -54,18 +55,6 @@ public class ItemEffect : MessageParserEffect
         args[0] = args[0][2..^2];
         args[1] = args[1][2..^2];
         var id = $"{args[0]};{args[1]}";
-        if (!CustomAssetsItemCache.TryGetValue(id, out var value))
-        {
-            CustomAssetsItemCache[id] = value = SaveType<string>.Load(SaveId, Default).CompileRichText(
-                new Dictionary<string, Action<RichTextLabel, string[]>>
-                {
-                    ["img"] = (l, _) => l.AddImage(
-                        CustomAssets.ItemImage(args[0], args[1], args[0], _ => reloadFunction()), 0, 20
-                    ),
-                    ["name"] = (l, _) => l.AddText(args[1]),
-                }, false
-            );
-        }
 
         label.PushContext();
         if (args.Length > 2 && int.TryParse(args[2], out var flagsRaw))
@@ -86,7 +75,17 @@ public class ItemEffect : MessageParserEffect
                 label.PushBgcolor(flags.GetBgColorFromItemFlag());
             }
         }
-        label.ApplyCompiledPrintableObjs(value);
+        label.ApplyCompiledPrintableObjs(
+            SaveType<string>.Load(SaveId, Default).CompileRichText(
+                new Dictionary<string, Action<RichTextLabel, string[]>>
+                {
+                    ["img"] = (l, _) => l.AddImage(
+                        CustomAssets.ItemImage(args[0], args[1], args[0], _ => reloadFunction()), 0, 20
+                    ),
+                    ["name"] = (l, _) => l.AddText(args[1]),
+                }, false
+            )
+        );
         label.PopContext();
     }
 }
